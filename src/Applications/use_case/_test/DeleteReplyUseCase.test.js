b8b9cb7d6321512247a4b90c759de2d18846b070
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-17 09:55:31 +0700

    Add unit test for DeleteReplyUseCase

---
+++ src/Applications/use_case/_test/DeleteReplyUseCase.test.js
@@ -0,0 +1,54 @@
+const DeleteReplyUseCase = require('../DeleteReplyUseCase')
+const ReplyRepository = require('../../../Domains/replies/ReplyRepository')
+
+describe('DeleteReplyUseCase', () => {
+    it('should orchestrating delete reply correctly', async () => {
+        // Arrange
+        const params = {
+            commentId: 'comment-123',
+            threadId: 'thread-123',
+            replyId: 'reply-123',
+        }
+
+        const owner = 'user-123'
+
+        // Mocking
+        const mockReplyRepository = new ReplyRepository()
+
+        mockReplyRepository.verifyReplyIsExist = jest
+            .fn()
+            .mockImplementation(() => Promise.resolve())
+
+        mockReplyRepository.verifyReplyOwner = jest
+            .fn()
+            .mockImplementation(() => Promise.resolve())
+
+        mockReplyRepository.deleteReplyById = jest
+            .fn()
+            .mockImplementation(() => Promise.resolve())
+
+        // create use case instance
+        const deleteReplyUseCase = new DeleteReplyUseCase({
+            replyRepository: mockReplyRepository,
+        })
+
+        // Action
+        await deleteReplyUseCase.execute(params, owner)
+
+        // Assert
+        expect(mockReplyRepository.verifyReplyIsExist).toBeCalledWith({
+            threadId: params.threadId,
+            commentId: params.commentId,
+            replyId: params.replyId,
+        })
+
+        expect(mockReplyRepository.verifyReplyOwner).toBeCalledWith({
+            replyId: params.replyId,
+            owner,
+        })
+
+        expect(mockReplyRepository.deleteReplyById).toBeCalledWith(
+            params.replyId
+        )
+    })
+})
