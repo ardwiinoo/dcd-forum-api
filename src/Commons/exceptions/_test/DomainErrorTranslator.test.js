aa7264cf4af92f2b600d4dad6c2a28fc05bb7832
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-17 09:55:46 +0700

    Refactor DomainErrorTranslator to include error messages for adding replies to comments

--- src/Commons/exceptions/_test/DomainErrorTranslator.test.js
+++ src/Commons/exceptions/_test/DomainErrorTranslator.test.js
@@ -84,6 +84,24 @@ describe('DomainErrorTranslator', () => {
                 'tidak dapat membuat comment baru karena tipe data tidak sesuai'
             )
         )
+        expect(
+            DomainErrorTranslator.translate(
+                new Error('ADD_REPLY.NOT_CONTAIN_NEEDED_PROPERTY')
+            )
+        ).toStrictEqual(
+            new InvariantError(
+                'tidak dapat membuat balasan comment baru karena properti yang dibutuhkan tidak ada'
+            )
+        )
+        expect(
+            DomainErrorTranslator.translate(
+                new Error('ADD_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION')
+            )
+        ).toStrictEqual(
+            new InvariantError(
+                'tidak dapat membuat balasan comment baru karena tipe data tidak sesuai'
+            )
+        )
     })
 
     it('should return original error when error message is not needed to translate', () => {
