ab2f34d466f4939fd9a9ab39a0cf3f4e8a8aa57a
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-17 09:55:20 +0700

    Refactor GetThreadUseCase to include replies in the returned thread

--- src/Applications/use_case/GetThreadUseCase.js
+++ src/Applications/use_case/GetThreadUseCase.js
@@ -1,21 +1,40 @@
 class GetThreadUseCase {
-    constructor({ threadRepository, commentRepository }) {
+    constructor({ threadRepository, commentRepository, replyRepository }) {
         this._threadRepository = threadRepository
         this._commentRepository = commentRepository
+        this._replyRepository = replyRepository
     }
 
     async execute(useCaseParams) {
         const { threadId } = useCaseParams
 
         const thread = await this._threadRepository.getThreadById(threadId)
-
         const comments = await this._commentRepository.getCommentsByThreadId(
             threadId
         )
+        const replies = await this._replyRepository.getRepliesByThreadId(
+            threadId
+        )
 
-        thread.comments = comments
+        thread.comments = this._getRepliesForComment(comments, replies)
 
         return thread
+    }
+
+    _getRepliesForComment(comments, replies) {
+        comments.forEach((comment) => {
+            const filteredReplies = replies
+                .filter((reply) => reply.commentId === comment.id)
+                .map((reply) => {
+                    delete reply.commentId
+
+                    return reply
+                })
+
+            comment.replies = filteredReplies
+        })
+
+        return comments
     }
 }
 
