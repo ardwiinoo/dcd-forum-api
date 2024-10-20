2e2c639227d35fe1ae23648c2ceac3216835939b
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-20 13:05:26 +0700

    Refactor ThreadRepositoryPostgres.js for improved code formatting and functionality

--- src/Infrastructures/repository/ThreadRepositoryPostgres.js
+++ src/Infrastructures/repository/ThreadRepositoryPostgres.js
@@ -1,4 +1,3 @@
-const DetailThread = require('../../Domains/threads/entities/DetailThread')
 const ThreadRepository = require('../../Domains/threads/ThreadRepository')
 const NotFoundError = require('../../Commons/exceptions/NotFoundError')
 const AddedThread = require('../../Domains/threads/entities/AddedThread')
@@ -12,19 +11,22 @@ class ThreadRepositoryPostgres extends ThreadRepository {
 
     async addThread(newThread) {
         const { title, body, owner } = newThread
+        const date = new Date().toISOString()
         const id = `thread-${this._idGenerator()}`
 
         const query = {
-            text: 'INSERT INTO threads VALUES($1, $2, $3, $4) RETURNING id, title, body, owner, date',
-            values: [id, title, body, owner],
+            text: 'INSERT INTO threads (id, title, body, owner, date) VALUES ($1, $2, $3, $4, $5) RETURNING id, title, owner',
+            values: [id, title, body, owner, date],
         }
 
         const { rows } = await this._pool.query(query)
 
-        return new AddedThread({ ...rows[0] })
+        return new AddedThread({
+            ...rows[0],
+        })
     }
 
-    async validateThreadById(threadId) {
+    async validateThreadAvailability(threadId) {
         const query = {
             text: 'SELECT * FROM threads WHERE id = $1',
             values: [threadId],
@@ -33,27 +35,25 @@ class ThreadRepositoryPostgres extends ThreadRepository {
         const { rowCount } = await this._pool.query(query)
 
         if (!rowCount) {
-            throw new NotFoundError('Thread tidak ditemukan')
+            throw new NotFoundError('thread tidak ditemukan')
         }
     }
 
     async getThreadById(threadId) {
         const query = {
-            text: 'SELECT t.id, t.title, t.body, t.date, u.username FROM threads t JOIN users u ON u.id = t.owner WHERE t.id = $1',
+            text: `SELECT t.id, t.title, t.body, t.date, u.username 
+            FROM threads AS t INNER JOIN users AS u 
+            ON t.owner=u.id WHERE t.id = $1`,
             values: [threadId],
         }
 
         const { rows, rowCount } = await this._pool.query(query)
 
         if (!rowCount) {
-            throw new NotFoundError('Thread tidak ditemukan')
+            throw new NotFoundError('thread tidak ditemukan')
         }
 
-        return new DetailThread({
-            ...rows[0],
-            date: rows[0].date.toISOString(),
-            comments: [],
-        })
+        return rows[0]
     }
 }
 
