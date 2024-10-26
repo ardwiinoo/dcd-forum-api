5e27af1c44ead6b2103375b843ba692b65dbb3d7
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-26 20:30:19 +0700

    Add LikeRepository.test.js for testing the LikeRepository interface

---
+++ src/Domains/likes/_test/LikeRepository.test.js
@@ -0,0 +1,22 @@
+const LikeRepository = require('../LikeRepository')
+
+describe('LikeRepository interface', () => {
+    it('should throw error when invoke abstract behavior', async () => {
+        // Arrange
+        const likeRepository = new LikeRepository()
+
+        // Action and Assert
+        await expect(likeRepository.likeComment({})).rejects.toThrowError(
+            'LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED'
+        )
+        await expect(likeRepository.unlikeComment({})).rejects.toThrowError(
+            'LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED'
+        )
+        await expect(likeRepository.verifyLikeComment({})).rejects.toThrowError(
+            'LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED'
+        )
+        await expect(
+            likeRepository.getLikeCountByCommentId('')
+        ).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+    })
+})
